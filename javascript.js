6c2c85cd88e1747b6bde85a74cfda0dffdcedc6a
Anubhav Baghel <136244141+anubhavbaghel23@users.noreply.github.com.>
2025-06-10 11:57:44 +0530

    Added color change feature in each div on hover.

--- javascript.js
+++ javascript.js
@@ -3,6 +3,12 @@ const container = document.querySelector('.container');
 const createGridButton = document.getElementById('createButton');
 
 
+function randomColorSelector(){
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
 
 
 function createGrid(){
@@ -10,13 +16,19 @@ function createGrid(){
     const columns = parseInt(document.getElementById('columnInput').value);
     const rows = parseInt(document.getElementById('rowInput').value);
 
+    container.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+
     const totalSquares = columns * rows;
 
     for(let i = 1; i < totalSquares; i++){
 
-        if(columns <= 100 || rows <= 100){
+        if(columns <= 100 && rows <= 100){
         const squareDiv = document.createElement('div');
         squareDiv.classList.add('squareDiv');
+
+        squareDiv.addEventListener('mouseenter', () => {
+            squareDiv.style.backgroundColor = randomColorSelector();
+        });
 
         container.appendChild(squareDiv);
         } else {
@@ -24,7 +36,8 @@ function createGrid(){
         } 
     }
 
-    container.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
-
+    
 
 }
+
+
